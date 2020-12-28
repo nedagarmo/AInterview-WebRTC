2b7cdcce5872f82e6c3e11b1d76a92a7515e249d
Nelson Garzón <nedagarmo@gmail.com>
2020-12-28 10:44:25 -0500

    fix: remove cors policy

--- app/server.js
+++ app/server.js
@@ -5,20 +5,12 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
-var fileServer = new(nodeStatic.Server)({
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, GET',
-    'Access-Control-Allow-Headers': 'Content-Type'
-  }
-});
+var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
 
-var io = socketIO.listen(app, {
-    origins: '*:*'
-});
+var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
 
   // convenience function to log server messages on the client
